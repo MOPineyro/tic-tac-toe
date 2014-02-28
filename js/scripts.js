1c963fe3797c9b95cd9fcd6c1ec62e404c8a6841
Manuel Omar Pineyro & Benjamin Paul Dunphy <student@epicodus.com>
2014-02-27 16:30:07 -0800

    update scripts to include Space method find and Object Board

--- js/scripts.js
+++ js/scripts.js
@@ -33,13 +33,29 @@ var Space = {
 
   markBy : function(player) {
     this.markBy = player;
+  },
+
+  find : function(posx, posy) {
+    var matchedSpaces = Space.all.filter(function(space) {
+      return posx === space.xCoordinate && posy === space.yCoordinate;
+    });
+    console.log(matchedSpaces[0]);
+    return matchedSpaces[0];
   }
 };
 
+var Board = {
 
+  initialize : function(posx, posy) {
+    this.spaces = [];
+    for (var x = 1; x <= 3; x++) {
+      for (var y = 1; y <= 3; y++) {
+        this.spaces.push(Space.create(x,y));
 
-
-
+      };
+    };
+  }
+};
 
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -19,13 +19,12 @@ describe("Space", function() {
   describe("initialize", function() {
     it("is initialized with an x and y coordinate", function() {
       var testSpace = Object.create(Space);
-      testSpace = Object.create(Space);
       testSpace.initialize(1, 2);
       testSpace.xCoordinate.should.equal(1);
       testSpace.yCoordinate.should.equal(2);
     });
   });
-  
+
   describe("create", function() {
     it("Creates positions on a board", function() {
       var testSpace = Space.create(1,2);
@@ -41,10 +40,36 @@ describe("Space", function() {
       testSpace.markBy.should.equal(testPlayer);
     });
   });
+
+  describe("find", function() {
+    it("returns a space by its coordinates", function() {
+      var testSpace = Object.create(Space);
+      testSpace.initialize(1, 2);
+      // console.log(testSpace.xCoordinate);
+      testSpace.find(1,2).should.eql(testSpace);
+    });
+  });  
 });
 
 
+describe("Board", function() {
+  describe("initialize", function() {
+    it("Sets up the board with 9 empty Spaces", function() {
+      var testBoard = Object.create(Board);
+      // var testSpace = Space.create(1,1);
+      testBoard.initialize();
+      testBoard.spaces.should.equal[9];
+    });
 
+    it("Sets up the board with correct coordinates", function() {
+      var testBoard = Object.create(Board);
+      testBoard.initialize();
+      // console.log(testBoard.spaces[5]);
+      testBoard.spaces[5].xCoordinate.should.equal(2);
+      testBoard.spaces[5].yCoordinate.should.equal(3);
+    });
+  });
+});
 
 
 
